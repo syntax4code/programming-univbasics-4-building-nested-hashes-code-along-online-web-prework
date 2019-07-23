add4f2226b0dcfb74b40a6ff3c30e22e1d18f732
Warren Loy Harper II <warrenharper2@att.net>
2019-07-23 21:52:20 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy[:montague][]hero
+epic_tragedy[:montague][hero][]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
